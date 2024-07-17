601487964adf3cc26598b43eaeb31529af02287f
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-17 08:54:57 -0700

    Express Validator

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,16 @@ path: api/auth
 */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
+
 const { createUser } = require("../controllers/auth");
 
 const router = Router();
 
-router.post("/new", createUser);
+router.post(
+  "/new",
+  [check("name", "El nombre es obligatorio").not().isEmpty()],
+  createUser
+);
 
 module.exports = router;
