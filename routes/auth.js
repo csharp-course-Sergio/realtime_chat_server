db2de9b802a759cfbf6e0ffa202e3255729728eb
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-17 09:35:30 -0700

    Middleware personalizado

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,18 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 
 const { createUser } = require("../controllers/auth");
+const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
 
 router.post(
   "/new",
-  [check("name", "El nombre es obligatorio").not().isEmpty()],
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
+    check("email", "El correo es obligatorio").isEmail(),
+    validateFields,
+  ],
   createUser
 );
 
