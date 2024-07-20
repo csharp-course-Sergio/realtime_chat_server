fcc93c6233b583d29549c39721058f44f690a44a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-20 14:00:24 -0700

    Actualizar base de datos cuando un usuario se conecta

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,18 @@
 const { checkJWT } = require("../helpers/jwt");
 const { io } = require("../index");
+const { connectedUser, disconnectedUser } = require("../controllers/socket");
 
 // Mensajes de Sockets
 io.on("connection", (client) => {
   console.log("Cliente conectado");
 
   const [valid, uid] = checkJWT(client.handshake.headers["authorization"]);
-  
+
   if (!valid) return client.disconnect();
 
+  connectedUser(uid);
+
   client.on("disconnect", () => {
-    console.log("Cliente desconectado");
+    disconnectedUser(uid);
   });
 });
